39f6817b504ec72b38c5e8e61f9f8eca34b14c51
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2024-01-12 10:44:59 +0100

    style: 🎨 nice readme init (#7)
    * style: 🎨 nice readme init
    
    * chore(version): 🔧 📦️ prerelease version bump to v0.0.4-readme.0
    
    * chore: 🔧 link examples file to readme
    
    * chore: 🔧 add changes done by sync:readme to git commit
    
    * fix: 🐛 improve header
    
    * fix: 🐛 example typing
    
    * fix: 🐛 update readme
    
    * fix: 🐛 rename readme to lowercase
    
    ---------
    
    Co-authored-by: Evert De Spiegeleer <evert.despiegeleer@rightcrowd.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ examples/basic-usage.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod'
+import {
+  Server,
+  controller,
+  get,
+  extendZodWithOpenApi,
+  zApiOutput,
+  apiResponse
+} from '@zhttp/core'
+
+extendZodWithOpenApi(z)
+// ⬆ What this allows you to do is to optionally add OAS info
+// to a Zod validation schema using zodSchema.openapi(...)
+// If this Zod schema is used in the input or output of an endpoint,
+// the info provided will be included in the generated openapi spec.
+//
+// Exmaple:
+
+const zHelloResponse = zApiOutput(z.object({
+  greeting: z.string().openapi({ example: 'Hello Joske!' })
+})).openapi('HelloResponse')
+
+const helloController = controller('Hello')
+  .description('This controller says hello to everyone')
+
+helloController.endpoint(
+  get('/hello')
+    .input(z.object({
+      params: z.object({
+        name: z.string().optional()
+      })
+    }))
+    .response(zHelloResponse)
+    .handler(async (input) => {
+      return apiResponse({
+        // Both the input object ⬇ and the handler response are strongly typed :)
+        greeting: `Hello ${input.params.name ?? 'everybody'}!`
+      })
+    })
+)
+
+const server = new Server({
+  controllers: [
+    helloController
+  ]
+}, {
+  port: 3000,
+  oasInfo: {
+    title: 'A very cool api',
+    version: '1.0.0'
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+server.start()
