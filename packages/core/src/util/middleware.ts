83524298e7b34b73197e03052805133cfe138b0c
Evert De Spiegeleer <evert.despiegeleer@rightcrowd.com>
2024-01-24 17:02:51 +0100

    fix: 🐛 middleware wrapper improvements

--- packages/core/src/util/middleware.ts
+++ packages/core/src/util/middleware.ts
@@ -1,14 +1,19 @@
 import { type RequestHandler, type ErrorRequestHandler, type Request, type Response, type NextFunction } from 'express'
 import { isPromise } from 'node:util/types'
+import { loggerInstance } from './logger.js'
 
 export enum MiddlewareTypes {
   BEFORE,
   AFTER,
 }
 
-export type AsyncRequestHandler = (...args: Parameters<RequestHandler>) => Promise<ReturnType<RequestHandler>>
+export type AsyncRequestHandler = (
+  ...args: Parameters<RequestHandler>
+) => Promise<ReturnType<RequestHandler>>
 
-export type AsyncErrorRequestHandler = (...args: Parameters<ErrorRequestHandler>) => Promise<ReturnType<ErrorRequestHandler>>
+export type AsyncErrorRequestHandler = (
+  ...args: Parameters<ErrorRequestHandler>
+) => Promise<ReturnType<ErrorRequestHandler>>
 
 export type MiddlewareHandler =
   | RequestHandler
@@ -16,53 +21,93 @@ export type MiddlewareHandler =
   | AsyncRequestHandler
   | AsyncErrorRequestHandler
 
-function middlewareWrapper (middlewareHandler: MiddlewareHandler) {
-  return function (prevError: Error, req: Request, res: Response, next: NextFunction) {
-    try {
-      if (isPromise(middlewareHandler)) {
-        new Promise((resolve, reject) => {
-          const localNext: NextFunction = (...params) => {
-            resolve(...params)
-          }
-          if (middlewareHandler.arguments.length === 3) {
-            const m = middlewareHandler as AsyncRequestHandler
-            m(req, res, localNext).then(resolve).catch(reject)
-          } else {
-            const m = middlewareHandler as AsyncErrorRequestHandler
-            m(prevError, req, res, localNext).then(resolve).catch(reject)
-          }
-        }).then(next).catch(next)
-      }
-
-      if (middlewareHandler.length === 3) {
-        const m = middlewareHandler as RequestHandler
-        m(req, res, next)
-      } else {
-        const m = middlewareHandler as ErrorRequestHandler
-        m(prevError, req, res, next)
-      }
-    } catch (err) {
-      next(err)
-    }
-  }
-}
-
-interface MiddlewareProps {
+interface MiddlewareProps<Handler extends MiddlewareHandler> {
   name?: string
-  handler: MiddlewareHandler
+  handler: Handler
   type: MiddlewareTypes
 }
 
-export class Middleware {
-  constructor (private readonly options: MiddlewareProps) {}
+const log = loggerInstance.logger('zhttp:middlewareHandler')
+
+function middlewareWrapper<Handler extends MiddlewareHandler> (
+  middlewareProps: MiddlewareProps<Handler>
+): Handler {
+  const middlewareHandler = middlewareProps.handler
+  if (middlewareHandler.length === 3) {
+    return async function (req: Request, res: Response, next: NextFunction) {
+      if (res.headersSent) {
+        log.info(
+          `Exiting middleware ${middlewareProps.name} early, headers already sent`
+        )
+        next(); return
+      }
+      try {
+        if (isPromise(middlewareHandler)) {
+          await new Promise((resolve, reject) => {
+            const localNext: NextFunction = (...params) => {
+              resolve(...params)
+            }
+            const m = middlewareHandler as AsyncRequestHandler
+            m(req, res, localNext).then(resolve).catch(reject)
+          })
+            .then(next)
+            .catch(next); return
+        }
+        const m = middlewareHandler as RequestHandler
+        m(req, res, next)
+      } catch (err) {
+        next(err)
+      }
+    } as Handler
+  }
+
+  return async function (
+    prevError: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (res.headersSent) {
+      log.info(
+        `Exiting middleware ${middlewareProps.name} early, headers already sent`
+      )
+      next(); return
+    }
+    try {
+      if (isPromise(middlewareHandler)) {
+        await new Promise((resolve, reject) => {
+          const localNext: NextFunction = (...params) => {
+            resolve(...params)
+          }
+          const m = middlewareHandler as AsyncErrorRequestHandler
+          m(prevError, req, res, localNext).then(resolve).catch(reject)
+        })
+          .then(next)
+          .catch(next); return
+      }
+
+      const m = middlewareHandler as ErrorRequestHandler
+      m(prevError, req, res, next)
+    } catch (err) {
+      next(err)
+    }
+  } as Handler
+}
+
+export class Middleware<Handler extends MiddlewareHandler = MiddlewareHandler> {
+  constructor (private readonly options: MiddlewareProps<Handler>) {}
 
   get type () {
     return this.options.type
   }
 
   get handler () {
-    return middlewareWrapper(this.options.handler)
+    return middlewareWrapper(this.options)
   }
 }
 
-export const middleware = (options: MiddlewareProps) => new Middleware(options)
+export const middleware = <
+  Handler extends MiddlewareHandler = MiddlewareHandler,
+>(
+    options: MiddlewareProps<Handler>
+  ) => new Middleware<Handler>(options)
